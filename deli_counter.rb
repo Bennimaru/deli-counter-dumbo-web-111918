bd8ff06c517a4e97d8d5044868a3e2ce0ad3cd2e
Benson Yu <bennywhyu@gmail.com>
2018-11-16 02:40:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli
-    puts "The line is currently #{katz_deli.index(x)+1}"
   
-  else katz_deli.size == 0
-    return "The line is currently empty."
+  katz_deli.each do |x|
+    if katz_deli
+      puts "The line is currently #{katz_deli.index(x)+1}"
+    else katz_deli.size == 0
+      return "The line is currently empty."
+    end
   end
 end
 
